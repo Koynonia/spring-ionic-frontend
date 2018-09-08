5e298d52959936378728d16a74426d25c8a1c380
Fernando M. Oliveira <koynonia@gmail.com>
2018-09-08 18:22:28 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://udemy-spring.herokuapp.com",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/udemy-dev-spring-ionic"
 }
